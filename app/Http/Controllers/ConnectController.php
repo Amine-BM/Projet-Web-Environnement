134ee693c4d903227886c65315ac24fe042050c2
Amine-BM <68389532+Amine-BM@users.noreply.github.com>
2021-11-30 14:35:36 +0100

    inscription + connexion (view, controller, css)

--- app/Http/Controllers/ConnectController.php
+++ app/Http/Controllers/ConnectController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class ConnectController {
     public function connexion(){
         return view('connexion.connect');
@@ -11,7 +13,26 @@ class ConnectController {
         return view('connexion.inscription');
     }
 
-    // public function contact(){
-    //     return view('contact');
-    // }
+    public function handleConnexion(Request $request){
+        $user = [
+            'name' => $request->username,
+            'mdp' => $request->password
+        ];
+        dd($user);
+    }
+
+    public function handleInscription(Request $request){
+        $user = [
+            'firstname' => $request->firstname,
+            'lastname' => $request->lastname,
+            'login' => $request->login,
+            'email' => $request->email,
+            'password' => $request->password,
+            'age' => $request->age,
+            'tel' => $request->tel,
+            'locataire' => $request->locataire,
+            'proprietaire' => $request->proprietaire,
+        ];
+        dd($user);
+    }
 }
---
+++ public/js/connexion.js
@@ -0,0 +1,8 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!***********************************!*\
+  !*** ./resources/js/connexion.js ***!
+  \***********************************/
+console.warn("done");
+/******/ })()
+;
