20413e37f7169867de40624118d43389c7d195c6
AminHSN <60926591+AminHSN@users.noreply.github.com>
2021-12-01 16:30:20 +0100

    le menu barre des tâches

---
+++ resources/views/appartements/appartements.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('content')
+    <div><a href="appartements"> Appartements</a></div>
+@endsection
---
+++ resources/views/etreplusvert.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('content')
+    <div><a href="etreplusvert"> Vous trouverez ici une série de vidéo pour avoir des conseil d'utilisation de différents appareils</a></div>
+@endsection
