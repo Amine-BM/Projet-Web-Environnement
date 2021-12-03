13630fb1984951b8354c0ab50f8cdde84b1070c8
AminHSN <60926591+AminHSN@users.noreply.github.com>
2021-12-03 16:29:43 +0100

    modification bouton déconnexion (problème d'affichage entre la navbar et le bouton)

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -11,18 +11,21 @@
         display: flex; justify-content:flex-start; width:90%; background-color: whitesmoke; border-radius: 4px; margin: 24px;
     }
 
-    .deco{position: relative; top: 15px; left: 1150px; background-color: grey; border: none; color: white; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; border-radius: 8px;
-
+    .deco{
+        position: absolute; right: 15px; top: 15px; height: auto;
+        background-color: grey; border: none; color: white; padding: 10px 15px; text-align: center;
+        text-decoration: none; display: inline-block; font-size: 16px; border-radius: 4px;
     }
 
 
 </style>
 
-<div class="deco">
-    <a id="deconnexion" href="{{route('connect')}} "> Se deconnecter</a>
-</div>
+
 
 <div class="navBar">
+    <div class="deco">
+        <a id="deconnexion" href="{{route('connect')}} "> Se deconnecter</a>
+    </div>
     <nav>
         <p ><a onmouseover="mouseOver()" id="accueil" href="{{route('accueil')}} "> L'accueil</a></p>
         <p ><a id="contact" href="{{route('contact')}} ">Page contact</a></p>
