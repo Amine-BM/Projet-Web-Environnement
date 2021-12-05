1a3ad6fcc2983cf617fa8cf6139017dbe60bb2a7
marwaouahouah1 <84792182+marwaouahouah1@users.noreply.github.com>
2021-12-05 20:12:08 +0100

    page contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,21 +1,21 @@
 @extends('layouts.app')
 @section('content')
-   <div class="p-8">
-    <h1 class="title">Contactez-nous</h1>
+<div class="p-8">
+    <h1 class="titre">Contactez-nous</h1>
     <div class="p-4 flex flex-row justify-between">
         <div class="mt-8 ml-28 ">
             <img class="im" src="images/maroua.png" alt="photo Maroua">
-            <div class="title">
-                Maroua Ouahouah
-                Responsable RH 
-            </div>
+                <div class="title">
+                    Maroua Ouahouah
+                    Responsable RH 
+                </div>
         </div>
         <div class=" mt-8 mr-16">
             <img class="im" src="images/Amin.jpg" alt="photo Amin">
-            <div class="title">
-                Amin Hasnaoui
-                Directeur Général 
-            </div>
+                <div class="title">
+                    Amin Hasnaoui
+                    Directeur Général 
+                </div>
         </div>
     </div> 
     <div class="flex flex-row justify-center">
@@ -23,28 +23,31 @@
             <div>
                 <img class="im" src="images/Amine.png" alt="photo Amine">
             </div>
-            <div>
-                <p>Amine Chebah
+            <div class="title">
+                Amine Chebah
                     CEO
-                </p>
             </div>
         </div>
     </div>
-    <div class="p-8">
+<div class="p-8">
     <div class="flex flex-row">
-    <div class="flex flex-column">
-    <div class="mt-8 mr-16">
-            <img class="w-12 h-12" src="images/email.jpg" alt="icon mail">
-            <a href ="Controle.de.qualite.de.citoyennete@gouv.fr">
-            Contrôle de Qualité de Citoyenneté 
+        <div class="flex flex-column">
             <div class="mt-8 mr-16">
-            <img class="w-12 h-12" src="images/tele.png" alt="icon telephone">
-            <a href ="Controle.de.qualite.de.citoyennete@gouv.fr">
-            01.67.88.12.12
+                <img class="w-12 h-12" src="images/email.jpg" alt="icon mail">
+                <a href ="Controle.de.qualite.de.citoyennete@gouv.fr">
+                <div class="title">
+                    Contrôle de Qualité de Citoyenneté
+                </div>
+            <div class="mt-8 mr-16">
+                <img class="w-12 h-12" src="images/tele.png" alt="icon telephone">
+                <a href ="Controle.de.qualite.de.citoyennete@gouv.fr">
+                <div class="title">
+                    01.67.88.12.12
+                </div>
             </div>
         </div>
-        </div>
     </div>
- </div>  
+</div>
+</div>  
 
 @endsection
