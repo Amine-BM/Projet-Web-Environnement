009285b87bdb78802b500239c80f8e1ec5d62c8b
Amine-BM <68389532+Amine-BM@users.noreply.github.com>
2021-12-12 16:40:14 +0100

    test data in views

--- resources/views/ajoutPiece.blade.php
+++ resources/views/ajoutPiece.blade.php
@@ -1,4 +1,15 @@
 @extends('dashboard')
 @section('content')
     <h1>J'ajoute une pièce à mon appartement</h1>
+    @if ($appartement==null)
+        <h2>Vous n'avez aucun appartement enregistré</h2>
+        <h3>Veuillez d'abord ajouter un appartement pour pouvoir ajouter une pièce</h3>
+    @else
+        <SELECT name="appartement" size="1">
+            @foreach ($appartement as $app)
+                <OPTION id="{{$app->idAppartement}}" >{{$app->numeroBoite}}
+            @endforeach
+        </SELECT>
+        <h2>Vous pouvez ajouter un appartement</h2>
+    @endif
 @endsection
