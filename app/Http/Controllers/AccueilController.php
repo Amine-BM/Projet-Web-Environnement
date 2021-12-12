fb888fbc19b9be8743229d917b07dc904b95e335
Amine-BM <68389532+Amine-BM@users.noreply.github.com>
2021-12-12 16:41:16 +0100

    controller + views with data

--- app/Http/Controllers/AccueilController.php
+++ app/Http/Controllers/AccueilController.php
@@ -2,13 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Piece;
+use App\Models\Gestion;
+use App\Models\Appareil;
+use App\Models\TypePiece;
+use App\Models\Appartement;
+use App\Models\TypeAppareil;
+use Illuminate\Http\Request;
+
 class AccueilController {
     public function welcome(){
         return view('welcome');
     }
     
-    public function accueil(){
-        return view('accueil');
+    public function accueil(Request $request){
+        $user = $request->user();
+        $pivot = $user->immeubles[0]->pivot;
+        //affichage des biens
+        return view('accueil', compact(['user', 'pivot']));
     }
 
     public function connexion(){
@@ -39,8 +50,41 @@ class AccueilController {
         return view('appareils');
     }
 
-    public function simulations(){
-        return view('simulations');
+    public function simulations(Request $request){
+        $piece = array();
+        $pieces = array(); // toutes les pièces de chacun des appartements du user
+        $appartement = $request->user()->appartements;
+        if($appartement != null){
+            foreach($appartement as $app){
+                $tmp = $app->pieces;
+                if($tmp->count() > 0 ){
+                    foreach($tmp as $t){
+                        array_push($piece, $t);
+                    }
+                    array_push($pieces, $piece);
+                }
+            }
+        }
+        $appareil = array();
+        $appareils = array();
+        foreach($pieces as $ap){
+            foreach ($ap as $piecee) {
+                $tmp = $piecee->appareils;
+                if($tmp->count() > 0){
+                    foreach($tmp as $t){
+                        array_push($appareil, $t);
+                    }
+                    array_push($appareils, $appareil);
+                }
+            }
+        }
+        return view('simulations', compact('appareils'));
+    }
+
+    public function storeSimulation(Request $request){
+        dd($request->nom);
+        
+        dd('gestion créé');
     }
 
     public function creaImmeuble(){
@@ -55,16 +99,53 @@ class AccueilController {
         return view('profil');
     }
 
-    public function ajoutAppareil(){
-        return view('ajoutAppareil');
+    public function ajoutAppareil(Request $request){
+        $piece = array();
+        $pieces = array(); // toutes les pièces de chacun des appartements du user
+        $appartement = $request->user()->appartements;
+        if($appartement != null){
+            foreach($appartement as $app){
+                $tmp = $app->pieces;
+                if($tmp->count() > 0 ){
+                    array_push($pieces, $tmp);
+                }
+            }
+        }
+        $typeAppareil = TypeAppareil::all();
+        return view('ajoutAppareil', compact('pieces', 'typeAppareil'));
     }
 
-    public function ajoutPiece(){
-        return view('ajoutPiece');
+    public function storeAppareil(Request $request){
+        dd($request);
+        Appareil::create([
+
+        ]);
+        dd('appareil créé');
     }
 
-    public function biens(){
-        return view('biens');
+    public function ajoutPiece(Request $request){
+        //liste de tous ses appartements
+        $appartement = $request->user()->appartements;
+        //immeuble pour chaque appart
+        $typePiece = TypePiece::all();
+        return view('ajoutPiece', compact('appartement', 'typePiece'));
+    }
+
+    public function storePiece(Request $request){
+        dd($request);
+        Piece::create([
+
+        ]);
+        dd('piece créé');
+    }
+
+    public function biens(Request $request){
+        $appartement = $request->user()->appartements;
+        $immeuble = null;
+        if($request->user()->proprietaire == 1){
+            $immeuble = $request->user()->immeubles;
+        }
+        return view('biens', compact('appartement', 'immeuble'));
     }
 
     public function consultAppareil(){
@@ -72,6 +153,7 @@ class AccueilController {
     }
 
     public function consultConsoAppareil(){
+        //récupérer les gestions pour chaque appareil 
         return view('consultConsoAppareil');
     }
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AccueilController;
 use App\Http\Controllers\ConnectController;
@@ -33,11 +35,19 @@ Route::get('/maisons', [AccueilController::class, 'maisons'])->name('maisons');
 Route::get('/appartements', [AccueilController::class, 'appartements'])->name('appartements');
 Route::get('/etreplusvert', [AccueilController::class, 'etreplusvert'])->name('vert');
 Route::get('/appareils', [AccueilController::class, 'appareils'])->name('appareils');
+
 Route::get('/simulations', [AccueilController::class, 'simulations'])->name('simulations');
+Route::post('/simulations', [AccueilController::class, 'storeSimulation'])->name('storeSimulation');
+
 Route::get('/creaImmeuble', [AccueilController::class, 'creaImmeuble'])->name('creaImmeuble');
 Route::get('/consultStatut', [AccueilController::class, 'consultStatut'])->name('consultStatut');
+
 Route::get('/ajoutAppareil', [AccueilController::class, 'ajoutAppareil'])->name('ajoutAppareil');
+Route::post('/ajoutAppareil', [AccueilController::class, 'storeAppareil'])->name('storeAppareil');
+
 Route::get('/ajoutPiece', [AccueilController::class, 'ajoutPiece'])->name('ajoutPiece');
+Route::post('/ajoutPiece', [AccueilController::class, 'storePice'])->name('storePiece');
+
 Route::get('/biens', [AccueilController::class, 'biens'])->name('biens');
 Route::get('/consultAppareil', [AccueilController::class, 'consultAppareil'])->name('consultAppareil');
 Route::get('/consultConsoAppareil', [AccueilController::class, 'consultConsoAppareil'])->name('consultConsoAppareil');
@@ -46,7 +56,9 @@ Route::get('/consultConsoImmeuble', [AccueilController::class, 'consultConsoImme
 Route::get('/creaAppartement', [AccueilController::class, 'creaAppartement'])->name('creaAppartement');
 Route::get('/creaImmeuble', [AccueilController::class, 'creaImmeuble'])->name('creaImmeuble');
 Route::get('/profil', [AccueilController::class, 'profil'])->name('profil');
-Route::get('/simulation', [AccueilController::class, 'simulation'])->name('simulation');
+
+// Route::get('/simulation', [AccueilController::class, 'simulation'])->name('simulation');
+
 Route::get('/ajoutContrat', [AccueilController::class, 'ajoutContrat'])->name('ajoutContrat');
 Route::get('/suppContrat', [AccueilController::class, 'suppContrat'])->name('suppContrat');
 Route::get('/suppImmeuble', [AccueilController::class, 'suppImmeuble'])->name('suppImmeuble');
@@ -56,6 +68,7 @@ Route::get('/suppPiece', [AccueilController::class, 'suppPiece'])->name('suppPie
 
 
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function (Request $request) {
+    $user = $request->user();
     return view('dashboard');
 })->name('dashboard');
